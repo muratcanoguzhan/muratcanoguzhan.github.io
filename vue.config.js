4f2dc96d7a66c66abd9e94fbd9388bb02a84c62f
Murat Oğuzhan <m.c.ogzhan@gmail.com>
2025-04-01 15:46:11 +0200

    Update Vue config for GitHub Pages deployment

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   
+  // Set the correct publicPath for GitHub Pages
+  // This should be the name of your repo in production, but development needs '/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/blog-app/'
+    : '/',
+  
   // Configure webpack-dev-server to properly serve .md files
   devServer: {
     static: {
