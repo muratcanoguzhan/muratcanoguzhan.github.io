fca5367cf7e3e2250dc5452c2cdb76b62fc6c47c
Murat Oğuzhan <m.c.ogzhan@gmail.com>
2025-04-02 02:06:45 +0200

    Revert changes to router and publicPath configuration, include deploy.yml changes

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   
-  // Set the correct publicPath for GitHub Pages
-  publicPath: './',
+  // Set the correct publicPath for custom domain
+  publicPath: '/',
   
   // Configure webpack-dev-server to properly serve .md files
   devServer: {
